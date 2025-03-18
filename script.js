c8cc6cae95f66bc34c4d2aac0110b7143be784e0
Hippoezio <inicoolaas@live.com>
2025-03-17 21:24:06 -0300

    Agregue un nuevo boton que elimina el ultimo caracter

--- script.js
+++ script.js
@@ -12,3 +12,8 @@ function agregarALaPantalla(value) {
     document.getElementById('pantalla').value = '';
   }
   
+  function deshacer() {
+    const valorPantalla = document.getElementById('pantalla').value;
+    const resultado = valorPantalla.substring(0, valorPantalla.length -1);
+    document.getElementById('pantalla').value = resultado;
+  }
