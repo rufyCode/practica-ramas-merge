5690ceb98e27021e87742fe6114529c8c9b4587f
rufyCode <idiablitoo@gmail.com>
2025-03-17 21:49:23 -0300

    Se borra el metodo ya que no se utiliza

--- script.js
+++ script.js
@@ -12,7 +12,3 @@ function agregarALaPantalla(value) {
     document.getElementById('pantalla').value = '';
   }
   
-  function deshacer() {
-    const valorPantalla = document.getElementById('pantalla').value;
-    document.getElementById('pantalla').value = valorPantalla.slice(0,-1);
-  }
